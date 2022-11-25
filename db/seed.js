e67d0b7e147ef4c2a8a8f22899fc089f1dd3b83a
aiyamadeline <aiyaedwards123@gmail.com>
2022-11-25 14:27:36 +0000

    added all questions

--- db/seed.js
+++ db/seed.js
@@ -42,110 +42,30 @@ async function seed() {
           {
             question: "Which animal can be seen on the Porsche logo?",
             answer: "Horse"
-          }
-        //       {
-        //         answerText: "Fox",
-        //         correctAnswer: false,
-        //       },
-        //       {
-        //         answerText: "Wolf",
-        //         correctAnswer: false,
-        //       },
-        //       {
-        //         answerText: "Horse",
-        //         correctAnswer: true,
-        //       },
-        //       {
-        //         answerText: "Bull",
-        //         correctAnswer: false,
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     questionText: "What is the name of the largest ocean on earth?",
-        //     answers: [
-        //       {
-        //         answerText: "Pacific Ocean",
-        //         correctAnswer: true,
-        //       },
-        //       {
-        //         answerText: "Atlantic Ocean",
-        //         correctAnswer: false,
-        //       },
-        //       {
-        //         answerText: "Indian Ocean",
-        //         correctAnswer: false,
-        //       },
-        //       {
-        //         answerText: "Artic Ocean",
-        //         correctAnswer: false,
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     questionText:
-        //       "Demolition of the Berlin wall separating East and West Germany began in what year?",
-        //     answers: [
-        //       {
-        //         answerText: "1980",
-        //         correctAnswer: false,
-        //       },
-        //       {
-        //         answerText: "1989",
-        //         correctAnswer: true,
-        //       },
-        //       {
-        //         answerText: "1992",
-        //         correctAnswer: false,
-        //       },
-        //       {
-        //         answerText: "1990",
-        //         correctAnswer: false,
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     questionText: "What is the rarest M&M color?",
-        //     answers: [
-        //       {
-        //         answerText: "Brown",
-        //         correctAnswer: true,
-        //       },
-        //       {
-        //         answerText: "Green",
-        //         correctAnswer: false,
-        //       },
-        //       {
-        //         answerText: "Red",
-        //         correctAnswer: false,
-        //       },
-        //       {
-        //         answerText: "Blue",
-        //         correctAnswer: false,
-        //       },
-        //     ],
-        //   },
-        //   {
-        //     questionText: "Which country invented ice cream?",
-        //     answers: [
-        //       {
-        //         answerText: "India",
-        //         correctAnswer: false,
-        //       },
-        //       {
-        //         answerText: "China",
-        //         correctAnswer: true,
-        //       },
-        //       {
-        //         answerText: "England",
-        //         correctAnswer: false,
-        //       },
-        //       {
-        //         answerText: "Brazil",
-        //         correctAnswer: false,
-        //       },
-        //     ],
-        //   },
+          },
+          
+        
+          {
+            question: "What is the name of the largest ocean on earth?",
+            answers: "Pacific Ocean"
+              
+          },
+          {
+            question:
+              "Demolition of the Berlin wall separating East and West Germany began in what year?",
+            answers:  "1989"
+             
+          },
+          {
+            question: "What is the rarest M&M color?",
+            answers: "Brown"
+          },
+              
+          {
+            question: "Which country invented ice cream?",
+            answers: "China"
+          
+          },
     ])
 }
 
