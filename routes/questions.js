1693ae18e341e0cb1bbb5e031e4255ccf65651c2
chloë js <91985648+chloe-js@users.noreply.github.com>
2022-11-25 10:20:12 +0000

    add alernative options with Get request

--- routes/questions.js
+++ routes/questions.js
@@ -8,8 +8,15 @@ questionsRouter.get('/', async (req, res) => {
      res.send(questions)
 })
 
+//This works with Postman from Questions findAll
+// const Questions = require("../models/Questions");
+// const Router = require('express');
+// const questionsRouter = Router();
 
-
+// //GET // http://localhost:5000/questions
+// questionsRouter.get('/', async (req, res) => {
+//      res.send(await Questions.findAll())
+// })
 
 
 module.exports = questionsRouter
--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,15 @@ const User = require("../models");
 const Router = require('express');
 const userRouter = Router();
 
+//GET // http://localhost:5000/user
+userRouter.get('/', async (req, res) => {
+    res.send(await User.findAll())
+})
 
+//GET // http://localhost:5000/1
 userRouter.get("/:userId", async (req, res) => {
     res.send(await User.findByPk(req.params.userId))
 })
-
-
-
 
 
 module.exports = userRouter
